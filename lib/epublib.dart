6a7c9e9240c9eeec8d82025feae6334c67e11e4d
Mensch272 <47662901+mensch272@users.noreply.github.com>
2021-12-28 07:36:21 +0500

    Initial commit

---
+++ lib/epublib.dart
@@ -0,0 +1,12 @@
+/// Easy interface to create epub files
+///
+/// Supports:
+/// - Images
+/// - Js
+/// - Fonts
+/// - Css
+library epublib;
+
+export 'src/enums.dart' show Namespace, EpubDirection;
+export 'src/models.dart';
+export 'src/writer.dart';
---
+++ lib/src/enums.dart
@@ -0,0 +1,30 @@
+enum ItemType {
+  unknown,
+  image,
+  style,
+  script,
+  navigation,
+  vector,
+  font,
+  video,
+  audio,
+  document,
+  cover,
+  smil,
+}
+
+enum Namespace {
+  dc,
+  opf,
+}
+
+enum EpubDirection {
+  ltr,
+  rtl,
+  lro,
+  rlo,
+}
+
+enum EpubProperties {
+  scripted,
+}
